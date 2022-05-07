ca2ad839b92b6774dd51dfe193c7ac98d5a7d6d7
Jimmy <jimmy.kha@mytwu.ca>
2022-05-07 09:00:56 -0700

    tutorialCompleted

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,75 @@
-const { auth } = require('express-openid-connect');
+let auth0 = null;
+const fetchAuthConfig = () => fetch("/auth_config.json");
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'qc3FtTn51PhANo7ZZSGbipaOjhfQIrqE',
-  issuerBaseURL: 'https://dev-m6axbjl4.us.auth0.com'
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
 };
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+window.onload = async () => {
+  await configureClient();
 
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+  updateUI();
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+      // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    // Process the login state
+    await auth0.handleRedirectCallback();
+    
+    updateUI();
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+}
+
+const updateUI = async () => { 
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+  
+  // NEW - add logic to show/hide gated content after authentication
+  if (isAuthenticated) {
+    document.getElementById("gated-content").classList.remove("hidden");
+
+    document.getElementById(
+      "ipt-access-token"
+    ).innerHTML = await auth0.getTokenSilently();
+
+    document.getElementById("ipt-user-profile").textContent = JSON.stringify(
+      await auth0.getUser()
+    );
+
+  } else {
+    document.getElementById("gated-content").classList.add("hidden");
+  }
+};
+
+
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+};
+
+
+// public/js/app.js
+
+const logout = () => {
+  auth0.logout({
+    returnTo: window.location.origin
+  });
+};
